030e1c03ac2d8f5b65846834628a25054b485580
ELSAYED MOHAMED <elsayed201@hotmail.com>
2022-11-11 00:54:58 +0200

    Set preferred device Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
